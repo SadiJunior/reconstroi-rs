f6b4da589fcf4eab35bab064b8d37832a3e467a8
Sadi Junior <sadilemesjr@gmail.com>
2024-05-26 14:06:04 -0300

    feat: added DataBase Context

---
+++ RenovaRS/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RenovaRS.Models;
+
+namespace RenovaRS.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
